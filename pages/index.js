16afd51dbe994461548b8f96c9d007056f7b835c
PV <40007694+paulvgithub@users.noreply.github.com>
2023-04-03 15:50:52 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,10 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app 543!" />
+        <Header title="Welcome to my app!" />
+    
+       <h1 style="color:brown">Quick brown fox</h>
+    
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
