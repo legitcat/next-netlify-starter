39d6fefe6f1dfa71dbfc856aa2d1c3bff280a04c
PV <40007694+paulvgithub@users.noreply.github.com>
2023-04-04 11:21:16 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,10 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
+const getTopStyle = () => {
+    return styles.topside;
+  };
+
 export default function Home() {
   return (
     <div className="container">
