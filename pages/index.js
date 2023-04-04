e1a3e6c5a3bf7b9461957e4f243cda042a05940d
PV <40007694+paulvgithub@users.noreply.github.com>
2023-04-04 08:06:59 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,7 @@ export default function Home() {
         <Header title="Welcome To Site!" />
     
        <h1>Quick brown fox trot</h1>
+       <Link href="/index.html> Index </Link>
     
         <p className="description">
           Get started by editing <code>pages/index.js</code>
