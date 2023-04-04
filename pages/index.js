11ee7d0b65a8b667df2a6ae09ca27d93e6a5b115
PV <40007694+paulvgithub@users.noreply.github.com>
2023-04-04 08:38:05 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,7 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-   
+        <link rel="icon" href="/favicon.ico" />   
       </Head>
 
       <main>
