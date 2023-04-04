88e3363fa86facb50d054ed7946d429ced48a08e
PV <40007694+paulvgithub@users.noreply.github.com>
2023-04-04 14:29:38 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,33 +11,37 @@ export default function Home() {
       </Head>
 
       <main>
+         
+         <body style="background-color:black">
       
-         <table>
-            <td>
-               <tr>
-                   <div className="container">
+             <table>
+                <td>
+                   <tr>
+                       <div className="container">
 
-                      <div className="topside-bin">
-                         <a href=""><div className='topside'></div></a>
-                      </div>
+                          <div className="topside-bin">
+                             <a href=""><div className='topside'></div></a>
+                          </div>
 
-                      <div className="both1">
-                         <a href="https://pvpython.pythonanywhere.com"><div className='side1'>&nbsp;MySQL</div></a>
-                         <a href="https://pvpython.pythonanywhere.com"><div className='side2'>&nbsp;Python</div></a>
-                      </div> 
+                          <div className="both1">
+                             <a href="https://pvpython.pythonanywhere.com"><div className='side1'>&nbsp;MySQL</div></a>
+                             <a href="https://pvpython.pythonanywhere.com"><div className='side2'>&nbsp;Python</div></a>
+                          </div> 
 
-                      <div className="stud1"></div>
-                      <div className="stud2"></div>
-                      <div className="stud3"></div>
-                      <div className="stud4"></div>
-                      <div className="stud5"></div>
-                      <div className="stud6"></div>
+                          <div className="stud1"></div>
+                          <div className="stud2"></div>
+                          <div className="stud3"></div>
+                          <div className="stud4"></div>
+                          <div className="stud5"></div>
+                          <div className="stud6"></div>
 
-                   </div>
-       
-                </tr>
-             </td>
-         </table>  
+                       </div>
+
+                    </tr>
+                 </td>
+             </table>  
+    
+         </body>
  
       </main>
 
