70d54cd33b4b9bb25258981a63c685fbc663915b
PV <40007694+paulvgithub@users.noreply.github.com>
2023-04-04 10:30:05 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 import Link from 'next/link';
-import styles from './styles.popup.css'
+import '../popup.css'
 
 export default function Home() {
   return (
