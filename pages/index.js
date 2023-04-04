c1138929b44b7f3e835ff372b48f97feaafb341c
PV <40007694+paulvgithub@users.noreply.github.com>
2023-04-04 11:41:19 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,13 +11,28 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome To Site!" />
       
-       <div classNam e="container">
-          <div className="topside-bin">
-             <a href=""><div className='topside'></div></a>
-          </div>
-       </div>
+         <div className="container">
+            <div className="topside-bin">
+               <a href=""><div className='topside'></div></a>
+            </div>
+    
+            <div className="both1">
+               <a style="font-size:28px;" href="https://pvpython.pythonanywhere.com"><div className='side1'>&nbsp;MySQL</div></a>
+               <a style="font-size:28px" href="https://pvpython.pythonanywhere.com"><div className='side2' align='right'>&nbsp;Python</div></a>
+            </div> 
+    
+         </div>
+    
+        <div className="stud1"></div>
+        <div className="stud2"></div>
+        <div className="stud3"></div>
+        <div className="stud4"></div>
+        <div className="stud5"></div>
+        <div className="stud6"></div>
+        <div className="stud7"></div>
+        <div className="stud8"></div>  
+    
    
       </main>
 
