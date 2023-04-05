bed2ca88faa29100e799e262a5db30d3262b7261
PV <40007694+paulvgithub@users.noreply.github.com>
2023-04-05 15:31:54 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,21 +13,30 @@ export default function Home() {
 
       <main>    
 
-              <div className='topside'>
-              </div>
 
-              <div className="both1">
-                 <a style="font-size:28px;" href="/api/ticker"><div className='side1'> Ticker</div></a>
-                 <a style="font-size:28px" href="/api/ticker"><div className='side2' align='right'>API</div></a>
-              </div>      
+           <div class="container">
+            <div class="brick" style="background-color:black;">
 
-              <div className="stud1"></div>
-              <div className="stud2"></div>
-              <div className="stud3"></div>
-              <div className="stud4"></div>
-              <div className="stud5"></div>
-              <div className="stud6"></div>
+                <div class='topside-bin'>
+                  <a style="font-size:30px;" href=""><div class='topside'></div></a>
+                </div>
 
+                <div className="both1">
+                   <a style="font-size:28px;" href="/api/ticker"><div className='side1'> Ticker</div></a>
+                   <a style="font-size:28px" href="/api/ticker"><div className='side2' align='right'>API</div></a>
+                </div>      
+
+                <div className="stud1"></div>
+                <div className="stud2"></div>
+                <div className="stud3"></div>
+                <div className="stud4"></div>
+                <div className="stud5"></div>
+                <div className="stud6"></div>
+    
+             </div>
+
+           </div>
+    
  
       </main>
 
