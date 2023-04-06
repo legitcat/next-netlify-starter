9a28bbb74d692cc156e5b1e3d2b1cea9a38ddea2
PV <40007694+paulvgithub@users.noreply.github.com>
2023-04-06 13:25:48 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,9 +19,8 @@ export default function Home() {
                 </div> 
     
                 <div class="both2">
-                   <a href="https://wired.com"><div className='side4'>Wired&nbsp;</div></a>
-                   <a href="https://wired.com"><div className='side3'>Mag&nbsp;</div></a>
-
+                   <a href="https://wired.com"><div className='side3'>Wired&nbsp;</div></a>
+                   <a href="https://wired.com"><div className='side4'>&nbsp;Mag</div></a>
                 </div>
 
                 <div className="stud1"></div>
