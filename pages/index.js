201e2bd38b942d9c5381ed1dad6739db5111356f
PV <40007694+paulvgithub@users.noreply.github.com>
2023-04-06 11:05:42 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,26 +14,23 @@ export default function Home() {
       </Head>
 
       <main> 
-    
-          <div className="brick">
 
-              <div className='topside-bin'>
-                <a href=""><div className='topside'></div></a>
-              </div>
+            <div className='topside-bin'>
+              <a href=""><div className='topside'></div></a>
+            </div>
 
-              <div className="both1">
-                 <a href="https://pvpython.pythonanywhere.com"><div className='side1'> MySql</div></a>
-                 <a href="https://pvpython.pythonanywhere.com"><div className='side2'>Python</div></a>
-              </div> 
+            <div className="both1">
+               <a href="https://pvpython.pythonanywhere.com"><div className='side1'> MySql</div></a>
+               <a href="https://pvpython.pythonanywhere.com"><div className='side2'>Python</div></a>
+            </div> 
 
-              <div className="stud1"></div>
-              <div className="stud2"></div>
-              <div className="stud3"></div>
-              <div className="stud4"></div>
-              <div className="stud5"></div>
-              <div className="stud6"></div>
+            <div className="stud1"></div>
+            <div className="stud2"></div>
+            <div className="stud3"></div>
+            <div className="stud4"></div>
+            <div className="stud5"></div>
+            <div className="stud6"></div>
 
-           </div> 
  
       </main>
 
