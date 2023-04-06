5a8e6b38aef13f4bcb4043177ae57263587c7cd9
PV <40007694+paulvgithub@users.noreply.github.com>
2023-04-06 13:34:12 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Hamburger from 'hamburger-react'
 
 export default function Home() {
   return (        
