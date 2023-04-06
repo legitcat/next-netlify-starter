babaa34e13d9a7f230aa9097cdd4bb7dc217e4c1
PV <40007694+paulvgithub@users.noreply.github.com>
2023-04-06 11:23:11 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,35 +5,39 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (    
     
-      <div>
-        <Head>
-          <title>Next.js Starter!</title>
-          <link rel="icon" href="/favicon.ico" />   
-        </Head>
+     <main>
+    
+          <div>
+            <Head>
+              <title>Next.js Starter!</title>
+              <link rel="icon" href="/favicon.ico" />   
+            </Head>
 
-        <main> 
-           <div className="brick">
-              <div className='topside-bin'>
-                <a href=""><div className='topside'></div></a>
-              </div>
+            <main> 
+               <div className="brick">
+                  <div className='topside-bin'>
+                    <a href=""><div className='topside'></div></a>
+                  </div>
 
-              <div className="both1">
-                 <a href="https://pvpython.pythonanywhere.com"><div className='side1'> MySql</div></a>
-                 <a href="https://pvpython.pythonanywhere.com"><div className='side2'>Python</div></a>
-              </div> 
+                  <div className="both1">
+                     <a href="https://pvpython.pythonanywhere.com"><div className='side1'> MySql</div></a>
+                     <a href="https://pvpython.pythonanywhere.com"><div className='side2'>Python</div></a>
+                  </div> 
 
-              <div className="stud1"></div>
-              <div className="stud2"></div>
-              <div className="stud3"></div>
-              <div className="stud4"></div>
-              <div className="stud5"></div>
-              <div className="stud6"></div>
-           </div>
+                  <div className="stud1"></div>
+                  <div className="stud2"></div>
+                  <div className="stud3"></div>
+                  <div className="stud4"></div>
+                  <div className="stud5"></div>
+                  <div className="stud6"></div>
+               </div>
 
-        </main>
+            </main>
 
-        <Footer />
-      </div>   
+            <Footer />
+          </div>  
+    
+      </main>
  
   )
 }
