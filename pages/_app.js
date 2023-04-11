7cd89ffcc3a71bfa80c1aac139a4612c2803f111
PaulV <40007694+paulvgithub@users.noreply.github.com>
2023-04-11 09:27:47 -0700

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import '@styles/globals.css'
+import '@styles/brick.css'
+import '@styles/menu.css'
 import '@styles/popup.css'
-
 
 function Application({ Component, pageProps }) {
   return <Component {...pageProps} />
